5a7c00af398c271e35374fa90e8fcf2e496dde64
kimnopal <naufalhakim366@gmail.com>
2023-12-22 16:54:00 +0700

    feat: add customable rating for each field and redesign ui

--- content.js
+++ content.js
@@ -1,25 +1,42 @@
 (() => {
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        const { command, saran, rating, nextLink } = request
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        const { command, saran, ratings, nextLink } = message
 
         if (command == 'run-page-1') {
-            const links = Array.from(document.querySelectorAll('#w5-container tbody tr [data-col-seq="7"] div a'), el => ({ link: el.href, status: el.textContent }))
-
-            for (const [index, link] of links.entries()) {
-                if (link.status != 'Sudah Isi') {
-                    sendResponse({ links, linkID: index })
-                    window.location.href = links[index].link
-                    break
-                }
-            }
+            const links = Array.from(document.querySelectorAll('#w5-container tbody tr [data-col-seq="7"] div a.btn'), 
+                el => el.textContent != 'Sudah Isi' ? ({ link: el.href, status: el.textContent }) : null
+            )
+            .filter(link => link != null)
+            
+            sendResponse({ links, linkID: 26 })
+            window.location.href = links[26].link
         }
 
         if (command == 'run-page-2') {
             (async () => {
-                const ratingButtons = Array.from(document.querySelectorAll(`.content table:nth-of-type(3) tbody input[value="${rating}"]`), el => el)
+                const ratingButtonsAll = Array.from(document.querySelectorAll(`.content table:nth-of-type(3) tbody input[type="radio"]`), el => el)
+                .reduce((prevValue, currentValue, index) => {
+                    if(index % 4 == 0 || index == 0) {
+                        prevValue.push([])
+                    }
+
+                    prevValue[prevValue.length - 1].push(currentValue)
+                    return prevValue
+                }, [])
+
+                console.log(ratingButtonsAll);
+
+                const ratingButtons = ratingButtonsAll.map((ratingButtons, index) => {
+                    return ratingButtons.filter(ratingButton =>  {
+                        return ratingButton.value == ratings[index]
+                    })[0]
+                })
+
+                console.log(ratingButtons);
 
                 const saranEl = document.querySelector('#saran')
                 const tombolSelesai = document.querySelector('#tombolselesai')
+
 
                 for (const ratingButton of ratingButtons) {
                     ratingButton.click()
@@ -28,7 +45,7 @@
 
                 saranEl.value = saran
 
-                tombolSelesai.click()
+                // tombolSelesai.click()
 
                 await new Promise((resolve, reject) => setTimeout(resolve, 1000));
                 window.location.href = nextLink || 'https://sia.akademik.unsoed.ac.id/krskhskuis/index'
--- popup/script.js
+++ popup/script.js
@@ -1,31 +1,46 @@
-document.querySelector('#run').addEventListener('click', async function () {
-    const saran = document.querySelector('#saran').value
-    const rating = document.querySelector('#rating:checked').value
-    const info = document.querySelector('.info')
+let saran, ratings, links, linkID
+const info = document.querySelector('.info')
+const run = document.querySelector('#run')
+let scrollDown = true
 
+run.addEventListener('click', async function () {
+    saran = document.querySelector('#saran').value
+    ratings = Array.from(document.querySelectorAll('.rating-container input[type="radio"]:checked'), el => el.value)
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    let { links, linkID } = await chrome.tabs.sendMessage(tab.id, {
+
+    const data = await chrome.tabs.sendMessage(tab.id, {
         command: 'run-page-1'
     });
 
-    chrome.tabs.onUpdated.addListener((tabId, change) => {
-        if (change.status == 'complete' && linkID < links.length) {
-            info.style.display = "block"
-            this.textContent = "Running"
+    links = data.links
+    linkID = data.linkID
+})
 
+chrome.tabs.onUpdated.addListener((tabId, change) => {
+    if (change.status == 'complete') {
+        info.style.display = "block"
+        run.textContent = "Running"
+        run.setAttribute("disabled", true)
+
+        if (scrollDown) {
+            window.scrollTo(0, document.body.scrollHeight);
+            scrollDown = false
+        }
+
+        if (linkID == links.length) {
+            info.style.display = "none"
+            run.textContent = "Run"
+            run.removeAttribute("disabled")
+        }
+
+        if (linkID < links.length) {
             linkID++
-
-            if (linkID >= links.length) {
-                info.textContent = "Selesai"
-                this.textContent = 'Run'
-            }
-
             chrome.tabs.sendMessage(tabId, {
                 command: "run-page-2",
                 saran,
-                rating,
+                ratings,
                 nextLink: links[linkID]?.link,
             })
         }
-    })
+    }
 })
